797b7868d2588fa6c549d9eac414e43b970f7438
michaeldonlon <michaeldonlon85@gmail.com>
2021-07-13 20:22:29 +1000

    chapter_15

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 # books/views.py
+from django.db.models import Q
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
@@ -28,3 +29,18 @@ class BookDetailView(
     context_object_name = 'book'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    
+
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+        # icontains ignores upper/lowercase
