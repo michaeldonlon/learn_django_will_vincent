f0d3a76bfd700c27cbdef766f3d8d414c4328a55
michaeldonlon <michaeldonlon85@gmail.com>
2021-07-15 16:07:27 +1000

    update heroku psql connection

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -239,5 +239,4 @@ if ENVIRONMENT == 'production':
 
 # Heroku
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
